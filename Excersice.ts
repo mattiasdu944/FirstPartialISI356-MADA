6d458a756787f6f63c4844b14b20c23932288c26
Mattias <duartemattias3@gmail.com>
2023-09-23 10:46:34 -0400

    refactor class Book and Library

--- Excersice.ts
+++ Excersice.ts
@@ -8,54 +8,86 @@ hasta la notificación por correo electrónico a los usuarios. Asimismo, se ha o
 sin importar si es por título, autor o ISBN, complicando su implementación.
 
 Se ha identificado que la clase es muy difícil de mantener y modificar. Tu tarea es estudiar el código, identificar los problemas y
- considerar cómo podría refactorizarse para mejorar su diseño y estructura.*/
- class LibraryManagerEx {
-    books: any[] = [];
-    loans: any[] = [];
+ considerar cómo podría refactorizarse para mejorar su diseño y estructura.
+*/
+
+class Book {
+    constructor(public title: string, public author: string, public ISBN: string) {}
+}
+
+class Library {
+    private books: Book[] = [];
 
     addBook(title: string, author: string, ISBN: string) {
-        this.books.push({ title, author, ISBN });
+        const book = new Book(title, author, ISBN);
+        this.books.push(book);
     }
-
+    
     removeBook(ISBN: string) {
-        const index = this.books.findIndex(b => b.ISBN === ISBN);
-        if (index !== -1) {
-            this.books.splice(index, 1);
-        }
+        this.books = this.books.filter(book => book.ISBN !== ISBN);
     }
 
-    search(query: string) {
-        // Usa el mismo método para buscar por título, autor o ISBN
-        return this.books.filter(book => 
-            book.title.includes(query) || 
-            book.author.includes(query) || 
-            book.ISBN === query
-        );
+    searchByTitle(query: string) {
+        return this.books.filter(book => book.title.includes(query));
     }
 
-    loanBook(ISBN: string, userID: string) {
-        const book = this.books.find(b => b.ISBN === ISBN);
-        if (book) {
-            this.loans.push({ ISBN, userID, date: new Date() });
-            this.sendEmail(userID, `Has solicitado el libro ${book.title}`);
-        }
+    searchByAuthor(query: string) {
+        return this.books.filter(book => book.author.includes(query));
     }
 
-    returnBook(ISBN: string, userID: string) {
-        const index = this.loans.findIndex(loan => loan.ISBN === ISBN && loan.userID === userID);
-        if (index !== -1) {
-            this.loans.splice(index, 1);
-            this.sendEmail(userID, `Has devuelto el libro con ISBN ${ISBN}. ¡Gracias!`);
-        }
-    }
-
-    sendEmail(userID: string, message: string) {
-        console.log(`Enviando email a ${userID}: ${message}`);
-        // Implementación ficticia del envío de correo
+    searchByISBN(ISBN: string) {
+        return this.books.find(book => book.ISBN === ISBN);
     }
 }
 
-const libraryex = new LibraryManagerEx();
-libraryex.addBook("El Gran Gatsby", "F. Scott Fitzgerald", "123456789");
-libraryex.addBook("1984", "George Orwell", "987654321");
-libraryex.loanBook("123456789", "user01");
+
+// class LibraryManagerEx {
+//     books: any[] = [];
+//     loans: any[] = [];
+
+//     addBook(title: string, author: string, ISBN: string) {
+//         this.books.push({ title, author, ISBN });
+//     }
+
+//     removeBook(ISBN: string) {
+//         const index = this.books.findIndex(b => b.ISBN === ISBN);
+//         if (index !== -1) {
+//             this.books.splice(index, 1);
+//         }
+//     }
+
+//     search(query: string) {
+//         // Usa el mismo método para buscar por título, autor o ISBN
+//         return this.books.filter(book =>
+//             book.title.includes(query) ||
+//             book.author.includes(query) ||
+//             book.ISBN === query
+//         );
+//     }
+
+//     loanBook(ISBN: string, userID: string) {
+//         const book = this.books.find(b => b.ISBN === ISBN);
+//         if (book) {
+//             this.loans.push({ ISBN, userID, date: new Date() });
+//             this.sendEmail(userID, `Has solicitado el libro ${book.title}`);
+//         }
+//     }
+
+//     returnBook(ISBN: string, userID: string) {
+//         const index = this.loans.findIndex(loan => loan.ISBN === ISBN && loan.userID === userID);
+//         if (index !== -1) {
+//             this.loans.splice(index, 1);
+//             this.sendEmail(userID, `Has devuelto el libro con ISBN ${ISBN}. ¡Gracias!`);
+//         }
+//     }
+
+//     sendEmail(userID: string, message: string) {
+//         console.log(`Enviando email a ${userID}: ${message}`);
+//         // Implementación ficticia del envío de correo
+//     }
+// }
+
+// const libraryex = new LibraryManagerEx();
+// libraryex.addBook("El Gran Gatsby", "F. Scott Fitzgerald", "123456789");
+// libraryex.addBook("1984", "George Orwell", "987654321");
+// libraryex.loanBook("123456789", "user01");
