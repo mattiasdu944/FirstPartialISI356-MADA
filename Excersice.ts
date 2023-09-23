70e774404a77b543003a424fb9b1088fb3f83253
Mattias <duartemattias3@gmail.com>
2023-09-23 10:57:19 -0400

    Loand and Email service

--- Excersice.ts
+++ Excersice.ts
@@ -40,52 +40,124 @@ class Library {
     }
 }
 
+interface IEmailService {
+    sendEmail(userID: string, message: string): void;
+}
 
-// class LibraryManagerEx {
-//     books: any[] = [];
-//     loans: any[] = [];
+class EmailService implements IEmailService {
+    sendEmail(userID: string, message: string) {
+        console.log(`Enviando email a ${userID}: ${message}`);
+        // Implementación real del envío de correo electrónico aquí
+    }   
+}
 
-//     addBook(title: string, author: string, ISBN: string) {
-//         this.books.push({ title, author, ISBN });
-//     }
+class LoansManager {
+    private loans: { ISBN: string; userID: string; date: Date }[] = [];
 
-//     removeBook(ISBN: string) {
-//         const index = this.books.findIndex(b => b.ISBN === ISBN);
-//         if (index !== -1) {
-//             this.books.splice(index, 1);
-//         }
-//     }
+    loanBook(ISBN: string, userID: string) {
+        const library = new Library();
+        const emailService = new EmailService();
 
-//     search(query: string) {
-//         // Usa el mismo método para buscar por título, autor o ISBN
-//         return this.books.filter(book =>
-//             book.title.includes(query) ||
-//             book.author.includes(query) ||
-//             book.ISBN === query
-//         );
-//     }
+        const book = library.searchByISBN(ISBN);
+        if (book) {
+            this.loans.push({ ISBN, userID, date: new Date() });
+            emailService.sendEmail(userID, `Has solicitado el libro ${book.title}`);
+        }
+    }
 
-//     loanBook(ISBN: string, userID: string) {
-//         const book = this.books.find(b => b.ISBN === ISBN);
-//         if (book) {
-//             this.loans.push({ ISBN, userID, date: new Date() });
-//             this.sendEmail(userID, `Has solicitado el libro ${book.title}`);
-//         }
-//     }
+    returnBook(ISBN: string, userID: string) {
+        const emailService = new EmailService();
 
-//     returnBook(ISBN: string, userID: string) {
-//         const index = this.loans.findIndex(loan => loan.ISBN === ISBN && loan.userID === userID);
-//         if (index !== -1) {
-//             this.loans.splice(index, 1);
-//             this.sendEmail(userID, `Has devuelto el libro con ISBN ${ISBN}. ¡Gracias!`);
-//         }
-//     }
+        const index = this.loans.findIndex(loan => loan.ISBN === ISBN && loan.userID === userID);
+        if (index !== -1) {
+            this.loans.splice(index, 1);
+            emailService.sendEmail(userID, `Has devuelto el libro con ISBN ${ISBN}. ¡Gracias!`);
+        }
+    }
 
-//     sendEmail(userID: string, message: string) {
-//         console.log(`Enviando email a ${userID}: ${message}`);
-//         // Implementación ficticia del envío de correo
-//     }
-// }
+}
+
+
+class User implements IObserver {
+    constructor(public userID: string) {}
+
+    update(message: string) {
+        console.log(`Usuario ${this.userID} ha recibido un mensaje: ${message}`);
+    }
+}
+
+interface IObserver {
+    update(message: string): void;
+}
+
+
+class NotificationService {
+    private observers: IObserver[] = [];
+
+    addObserver(observer: IObserver) {
+        this.observers.push(observer);
+    }
+
+    removeObserver(observer: IObserver) {
+        const index = this.observers.indexOf(observer);
+        if (index !== -1) {
+            this.observers.splice(index, 1);
+        }
+    }
+
+    notifyObservers(message: string) {
+        this.observers.forEach(observer => observer.update(message));
+    }
+}
+
+
+
+
+class LibraryManagerEx {
+    books: any[] = [];
+    loans: any[] = [];
+
+    addBook(title: string, author: string, ISBN: string) {
+        this.books.push({ title, author, ISBN });
+    }
+
+    removeBook(ISBN: string) {
+        const index = this.books.findIndex(b => b.ISBN === ISBN);
+        if (index !== -1) {
+            this.books.splice(index, 1);
+        }
+    }
+
+    search(query: string) {
+        // Usa el mismo método para buscar por título, autor o ISBN
+        return this.books.filter(book =>
+            book.title.includes(query) ||
+            book.author.includes(query) ||
+            book.ISBN === query
+        );
+    }
+
+    loanBook(ISBN: string, userID: string) {
+        const book = this.books.find(b => b.ISBN === ISBN);
+        if (book) {
+            this.loans.push({ ISBN, userID, date: new Date() });
+            this.sendEmail(userID, `Has solicitado el libro ${book.title}`);
+        }
+    }
+
+    returnBook(ISBN: string, userID: string) {
+        const index = this.loans.findIndex(loan => loan.ISBN === ISBN && loan.userID === userID);
+        if (index !== -1) {
+            this.loans.splice(index, 1);
+            this.sendEmail(userID, `Has devuelto el libro con ISBN ${ISBN}. ¡Gracias!`);
+        }
+    }
+
+    sendEmail(userID: string, message: string) {
+        console.log(`Enviando email a ${userID}: ${message}`);
+        // Implementación ficticia del envío de correo
+    }
+}
 
 // const libraryex = new LibraryManagerEx();
 // libraryex.addBook("El Gran Gatsby", "F. Scott Fitzgerald", "123456789");
